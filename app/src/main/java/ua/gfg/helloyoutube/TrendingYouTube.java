2074c8a8f680c0830a933a60f25952a247d1e10c
GrandCkay <cf.ckay@gmail.com>
2020-06-19 17:01:51 +0300

    final

--- app/src/main/java/ua/gfg/helloyoutube/TrendingYouTube.java
+++ app/src/main/java/ua/gfg/helloyoutube/TrendingYouTube.java
@@ -2,5 +2,10 @@ package ua.gfg.helloyoutube;
 
 public class TrendingYouTube {
     public static void main(String[] args) {
+        System.out.println("Music");
+        System.out.println("Gaming");
+        System.out.println("News");
+        System.out.println("Movies");
+        System.out.println("Fashion & Beauty");
     }
 }
