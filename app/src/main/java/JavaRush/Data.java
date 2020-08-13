68bcc22e11dee001ffee449e5a70b1d19939db91
GrandCkay <cf.ckay@gmail.com>
2020-08-13 22:14:20 +0300

    updating javaRush

---
+++ app/src/main/java/JavaRush/Data.java
@@ -0,0 +1,12 @@
+package JavaRush;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class Data {
+    public static void main(String[] args) {
+        SimpleDateFormat formatter = new SimpleDateFormat("dd MM yyyy");
+        Date date = new Date();
+        System.out.println(formatter.format(date));
+    }
+}
