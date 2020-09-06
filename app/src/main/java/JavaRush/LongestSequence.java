8c451db60b3c81a559a02447da2e8edc07c33e1d
GrandCkay <cf.ckay@gmail.com>
2020-09-06 11:43:22 +0300

    new video - JavaRush level 8 lesson 6

---
+++ app/src/main/java/JavaRush/LongestSequence.java
@@ -0,0 +1,38 @@
+package JavaRush;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+public class LongestSequence {
+    public static void main(String[] args) throws IOException {
+        List<Integer> arrayList = new ArrayList<>();
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        int count = 1;
+        int add = 1;
+
+        for (int i = 0; i < 10; i++) {
+            arrayList.add(Integer.parseInt(reader.readLine()));
+        }
+
+        for (int i = 1; i < arrayList.size(); i++) {
+            if (arrayList.get(i).equals(arrayList.get(i - 1))) {
+                add++;
+                count = setCount(add, count);
+            } else {
+                count = setCount(add, count);
+                add = 1;
+            }
+        }
+        System.out.println(count);
+    }
+
+    public static int setCount(int add, int count) {
+        if (add > count) {
+            count = add;
+        }
+        return count;
+    }
+}
