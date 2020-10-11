db7da836c61e1d62688e7d1638b56b47fb9db58d
GrandCkay <cf.ckay@gmail.com>
2020-10-11 22:38:01 +0300

    new video - JavaRush level 8 lesson 8 - 11

---
+++ app/src/main/java/JavaRush/AllAnimals.java
@@ -0,0 +1,64 @@
+package JavaRush;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class AllAnimals {
+    public static void main(String[] args) {
+        Set<Cat> cats = createCats();
+        Set<Dog> dogs = createDogs();
+
+        Set<Object> pets = join(cats, dogs);
+        printPets(pets);
+
+        removeCats(pets, cats);
+        printPets(pets);
+    }
+
+    public static Set<Cat> createCats() {
+        Set<Cat> result = new HashSet<>();
+        //напишите тут ваш код
+        for (int i = 0; i < 4; i++) {
+            result.add(new Cat());
+        }
+        return result;
+    }
+
+    public static Set<Dog> createDogs() {
+        //напишите тут ваш код
+        Set<Dog> resultDog = new HashSet<>();
+        for (int i = 0; i < 3; i++) {
+            resultDog.add(new Dog());
+        }
+        return resultDog;
+    }
+
+    public static Set<Object> join(Set<Cat> cats, Set<Dog> dogs) {
+        //напишите тут ваш код
+        Set<Object> allAnimal = new HashSet<>();
+        allAnimal.addAll(cats);
+        allAnimal.addAll(dogs);
+        return allAnimal;
+    }
+
+    public static void removeCats(Set<Object> pets, Set<Cat> cats) {
+        //напишите тут ваш код
+        pets.removeAll(cats);
+    }
+
+
+    public static void printPets(Set<Object> pets) {
+        //напишите тут ваш код
+        for (Object pet : pets) {
+            System.out.println(pet);
+        }
+    }
+
+
+    //напишите тут ваш код
+    public static class Cat {
+    }
+
+    public static class Dog {
+    }
+}
